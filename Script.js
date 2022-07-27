b5c5f752630da69487981f9d6b03ff52a6f22749
Avani14 <trivediavani14@gmail.com>
2022-07-27 12:06:36 +0530

    Added the ability to populate the object and displaying it on the page

--- Script.js
+++ Script.js
@@ -1,22 +1,50 @@
-window.addEventListener('DOMContentLoaded',(event)=>{
-    createInnerHTML();
-});
-const createInnerHTML = () =>{
-    const headerHTML = "<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>"
-    const innerHTML = `${headerHTML}
-    <tr>
-            <td>
-                <img class="profile" src="images/Ellipse -1.png" alt="">
-            </td>
-            <td>Avani Trivedi</td>
-            <td>Female</td>
-            <td> <div class="dept-label">
-                IT
-            </div> </td>
-            <td>500000</td>
-            <td>19 April 2021</td>
-            <td><img id = "1" src="images/delete-black-18dp.svg" alt="delete" onclick="remove(this)">
-                <img id = "1" src="images/create-black-18dp.svg" alt="update" onclick="update(this)"></td>
-        </tr>`;
-        document.querySelector('#display').innerHTML= innerHTML
-}
+const formDetails = () => {
+  var fname = document.getElementById("first-name").value;
+  var gender = document.querySelector('input[type=radio][name=gender]:checked').value;
+  var profile = document.getElementById("profile").value;
+  var dept = document.querySelector('input[type=checkbox][name=dept]:checked').value;
+  var salary = document.getElementById("salary").value;
+  var date = document.getElementById("start_date").value;
+  let empObj = {
+    fname: fname,
+    gender: gender,
+    profile: profile,
+    dept: dept,
+    salary: salary,
+    date: date,
+  };
+  var empMap = [];
+  empMap = JSON.parse(localStorage.getItem("empMap"));
+  if (empMap == null) {
+    empMap = [];
+  }
+  empMap.push(empObj);
+  localStorage.setItem("empMap", JSON.stringify(empMap));
+};
+const displayData = () => {
+  var table = document.getElementById("display");
+  var emp_details = [];
+  emp_details = JSON.parse(localStorage.getItem("empMap"));
+  emp_details.forEach((element) => {
+    var newRow = table.insertRow();
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    var cell3 = newRow.insertCell(2);
+    var cell4 = newRow.insertCell(3);
+    var cell5 = newRow.insertCell(4);
+    var cell6 = newRow.insertCell(5);
+    var cell7 = newRow.insertCell(6);
+    cell1.innerHTML = '<img class="avatar" src="'+element.profile+'" alt="" />';
+    cell2.innerHTML = element.fname;
+    cell3.innerHTML = element.gender;
+    cell4.innerHTML = element.dept;
+    cell5.innerHTML = element.salary;
+    cell6.innerHTML = element.date;
+    cell7.innerHTML =
+      '<img id = "1" src="images/delete-black-18dp.svg" alt="delete" onclick="delete(' +
+      element.fname +
+      ')"><img id = "1" src="images/create-black-18dp.svg" alt="update" onclick="update('+element.fname+')"></a>';
+  });
+  var x = document.getElementById("display").rows.length;
+  document.getElementById("emp_count").innerHTML = x - 1;
+};
